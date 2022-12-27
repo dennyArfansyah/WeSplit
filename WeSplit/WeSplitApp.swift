cc941a5f4c1e0b5a9d7988976ff03455e1594546
dennyArfansyah <dennyarfansyah@gmail.com>
2022-12-27 22:53:53 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Denny Arfansyah on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
