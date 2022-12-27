fbd0fe89f5d991d84dc6d69b47e819302b7bd2a7
dennyArfansyah <dennyarfansyah@gmail.com>
2022-12-28 00:14:03 +0700

    Finalizing code for day 1

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var pressCount = 0
+    
+    private var players = ["Neuer", "Gundogan", "Rudiger", "Kimich"]
+    @State private var selectedPlayer = "Kimich"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hallo, Willkommen nach SwiftUI")
+                }
+                
+                Section {
+                    TextField("Schreiben Sie Ihr Name", text: $name)
+                    Text("Mein Name ist \(name)")
+                }
+                Button("Press Me!: \(pressCount)") {
+                    pressCount += 1
+                }
+                Picker("Select your player", selection: $selectedPlayer) {
+                    ForEach(players, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .navigationTitle("Tag 1")
         }
-        .padding()
     }
 }
 
